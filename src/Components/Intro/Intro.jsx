6908e33c2bc7af883004ef246dae80a150a51780
Moumenh <mohanbali94@gmail.com>
2021-01-19 16:12:44 +0200

    name change

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -10,7 +10,7 @@ const Intro = () => {
             <div className='intro__details'>
                 <Fade left duration={1500}>
                     <h1 className='intro__name'>Hello✌🏼,</h1>
-                    <h1 className='intro__name2'>my name is <span className='color'>Mo'men Hanbali</span></h1>
+                    <h1 className='intro__name2'>I'm <span className='color'>Mo'men Hanbali</span></h1>
                 </Fade>
                 <Fade right delay={700} duration={1500}>
                     <h1 className='intro__Info'><span style={{ fontWeight: '200' }}>A Passionate</span> Full Stack Developer </h1>
